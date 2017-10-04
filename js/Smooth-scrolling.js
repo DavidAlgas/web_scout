e63ccfafab81d12ca78f0f5e6ada5d21673dd29b
David Algás <david_algas@hotmail.com>
2017-10-04 23:49:57 +0200

    Minor Update
    Unido js a un archivo solo y minificado

--- js/Smooth-scrolling.js
+++
@@ -1,1 +0,0 @@
-$('body').scrollspy({target:".navbar",offset:50});$("#navSuave a").on('click',function(event){if(this.hash!==""){event.preventDefault();var hash=this.hash;$('html, body').animate({scrollTop:$(hash).offset().top},800,function(){window.location.hash=hash;});}});
